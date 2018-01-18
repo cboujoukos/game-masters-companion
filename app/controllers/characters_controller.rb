fcd7b840e7c773bf20a719a14e4bbd625ce65ea7
Chris Boujoukos <github email address>
2018-01-18 10:01:45 -0500

    add character show and edit page

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -21,4 +21,35 @@ class CharactersController < ApplicationController
       redirect '/'
     end
   end
+
+  get "/campaigns/:slug/characters/:id" do
+    @user = User.find(session[:id])
+    @campaign = Campaign.find_by_slug(params[:slug])
+    @character = Character.find(params[:id])
+    if logged_in?
+      erb :'/characters/show'
+    else
+      redirect '/'
+    end
+  end
+
+  get "/campaigns/:slug/characters/:id/edit_character" do
+    @user = User.find(session[:id])
+    @campaign = Campaign.find_by_slug(params[:slug])
+    @character = Character.find(params[:id])
+    if logged_in?
+      erb :'/characters/edit'
+    else
+      redirect '/'
+    end
+  end
+
+  post "/campaigns/:slug/characters/:id" do
+    @user = User.find(session[:id])
+    @campaign = Campaign.find_by_slug(params[:slug])
+    @character = Character.find(params[:id])
+    @character.update(params[:characters][])
+    @character.save
+  end
+
 end
