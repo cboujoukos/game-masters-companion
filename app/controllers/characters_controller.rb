f58bf272556598159057d65c03a93f4511d7e9dd
Chris Boujoukos <github email address>
2018-01-18 10:50:46 -0500

    add delete button to character

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -53,4 +53,15 @@ class CharactersController < ApplicationController
     redirect "/campaigns/#{@campaign.slug}/characters/#{@character.id}"
   end
 
+  delete "/campaigns/:slug/characters/:id/delete" do
+    @campaign = Campaign.find_by_slug(params[:slug])
+    @character = Character.find(params[:id])
+    if current_user.id == session[:id]
+      @character.delete
+      redirect "campaigns/#{@campaign.slug}"
+    else
+      redirect '/'
+    end
+  end
+
 end
